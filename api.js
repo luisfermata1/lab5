b8a9d4ff0887aba0d2b5b684b8d493edf875feaa
luisfermata <luisfermata@icloud.com>
2018-10-07 17:41:58 -0600

    metodo delete

--- api.js
+++ api.js
@@ -39,6 +39,30 @@ app.get("/api/v1/pedido/:id?", (req, res) => {
     }
 });
 
+//Función DELETE
+app.delete("/api/v1/pedido/:id", (req, res) => {
+    var id = req.params.id;
+    var pedidos_unico = pedidos.pedidos.filter(x => x.id==id)
+    if( pedidos_unico.length>0 )
+    { 
+        var pedidos2 = [];
+        for(var i = 0; i < pedidos.pedidos.length; i++){
+            if(pedidos.pedidos[i].id != id)
+                pedidos2.push(pedidos.pedidos[i]);
+        }
+        pedidos.pedidos = pedidos2;
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.write("200 Delete Succesfully");
+        res.end();
+    }
+    else
+    {
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.write("404 Not found");
+        res.end();
+    }
+});
+
 //función para crear servidor
 function apiServer(){
     app.listen(3000, () => {
